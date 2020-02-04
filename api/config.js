229e9ea5adfd187e49aaeb9f172d8e9461fb46b2
João Paulo Santana <santanadeveloper@outlook.com>
2020-02-03 22:05:13 -0300

    Updates

--- api/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    USER: 'joao@agenciasantana.com.br', 
-    PASS: 'hiozkiehcowgfeoe'
-}
---
+++ precache-manifest.ce65242eb1a459aab1ee8a2aca827d5b.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893c728e868d371d4c7c1b8a38842293",
+    "url": "/deploy/index.html"
+  },
+  {
+    "revision": "efdf7312244dc9ad8884",
+    "url": "/deploy/static/css/main.ca098e0d.chunk.css"
+  },
+  {
+    "revision": "f0ec845401e11ac8ea1e",
+    "url": "/deploy/static/js/2.ab78782b.chunk.js"
+  },
+  {
+    "revision": "33680f10ac176f8b277c53fed91c1f3b",
+    "url": "/deploy/static/js/2.ab78782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efdf7312244dc9ad8884",
+    "url": "/deploy/static/js/main.49b0bee8.chunk.js"
+  },
+  {
+    "revision": "104520d74e0b5441003a",
+    "url": "/deploy/static/js/runtime-main.422d4866.js"
+  },
+  {
+    "revision": "d1c09e929fc5239ecca1f65bb2f6c4d9",
+    "url": "/deploy/static/media/agencia-santana-white.d1c09e92.svg"
+  },
+  {
+    "revision": "05b0b8ea649d4751f358ca2a88f95af2",
+    "url": "/deploy/static/media/agencia-santana.05b0b8ea.webp"
+  },
+  {
+    "revision": "769ea95c47e789708753e6c5d497aa63",
+    "url": "/deploy/static/media/clientes.769ea95c.webp"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/deploy/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/deploy/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/deploy/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/deploy/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/deploy/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4ffd4fe7945af123788bf5888296c696",
+    "url": "/deploy/static/media/github.4ffd4fe7.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/deploy/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "699aed41d894f2f81fea010214bc5eac",
+    "url": "/deploy/static/media/hero-agencia.699aed41.webp"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/deploy/static/media/ill-2.594b1ee1.svg"
+  },
+  {
+    "revision": "1e7c70dc7d471d845aeed9e9bead0fe5",
+    "url": "/deploy/static/media/img-1-1200x1000.1e7c70dc.jpg"
+  },
+  {
+    "revision": "ba449a0697388668ef05e043ffdeafca",
+    "url": "/deploy/static/media/img-2-1200x1000.ba449a06.jpg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/deploy/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/deploy/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/deploy/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/deploy/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/deploy/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "75f7ab18f707114d817522df8dea0a3b",
+    "url": "/deploy/static/media/promo-1.75f7ab18.png"
+  },
+  {
+    "revision": "052702d8cc3736586971f20d237abb1e",
+    "url": "/deploy/static/media/responsive-design.052702d8.svg"
+  },
+  {
+    "revision": "b184835c819cb03815915c976e53d0bc",
+    "url": "/deploy/static/media/seo.b184835c.svg"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/deploy/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "dcfcf3b77210fe85b0abc8260e6fa70e",
+    "url": "/deploy/static/media/team-2-800x800.dcfcf3b7.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/deploy/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/deploy/static/media/team-4-800x800.23007132.jpg"
+  },
+  {
+    "revision": "1902afd2a74e737ab3ecfa6b283ee8e9",
+    "url": "/deploy/static/media/uxui.1902afd2.svg"
+  }
+]);
